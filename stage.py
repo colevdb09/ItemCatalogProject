c68ad0c78fabbe4632b28469076bec57acc4f09f
Cole VandenBerg <colevdb09@gmail.com>
2018-10-17 07:51:10 -0700

    Added CRUD routes

--- stage.py
+++ stage.py
@@ -12,3 +12,26 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 
+@app.route('/')
+@app.route('/bands')
+def bands():
+
+@app.route('/bands/new', methods = ['GET','POST'])
+def newband():
+
+@app.route('/bands/<int:id>')
+
+@app.route('/bands/<int:id>/edit', methods = ['GET','POST'])
+def editband():
+
+@app.route('/bands/<int:id>/delete', methods = ['GET','DELETE'])
+def delband():
+
+@app.route('/bands/<int:id>/new', methods = ['GET','POST'])
+def newalbum():
+
+@app.route('/bands/<int:id>/<int:alb_id>/edit', methods = ['GET','POST'])
+def editalbum():
+
+@app.route('/bands/<int:id>/<int:alb_id>/delete', methods = ['GET','DELETE'])
+def deletealbum():
